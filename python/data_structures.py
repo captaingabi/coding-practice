d827250e7029ffc6c649db0af70bdab86446a8e3
Gabor Kapitany <captaingabi@gmail.com>
2024-07-21 23:39:34 +0200

    python: add queue

--- python/data_structures.py
+++ python/data_structures.py
@@ -1,8 +1,9 @@
 #!/bin/python3
 
 from collections import deque
+from queue import Queue
+from threading import Thread
 from typing import Deque
-
 # list      ordered     mutable     duplicates
 # queue     ordered     mutable     duplicates      FIFO
 # stack     ordered     mutable     duplicates      LIFO
@@ -28,3 +29,23 @@ try:
     print(myStack.pop())
 except IndexError:
     pass
+
+
+q: Queue = Queue()
+
+
+def worker():
+    while True:
+        item = q.get()
+        print(f'Working on {item}')
+        print(f'Finished {item}')
+        q.task_done()
+
+
+Thread(target=worker, daemon=True).start()
+
+for item in range(5):
+    q.put(item)
+
+q.join()
+print('All work completed')
